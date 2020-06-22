673673a3531b47ebd4321d8bfe3fdd264b3ffe45
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2020-06-22 18:02:05 +0530

    compare

--- src/CompareAll.py
+++ src/CompareAll.py
@@ -29,11 +29,9 @@ def compare():
       # n changes
     r_waiting,r_turn_around,r_compl_time = ROUND_ROBIN.findavgTime(processes, len(processes), burst_time,arrival_time, quantum) 
 
-    np_waiting , np_turn_around, np_compl_time = PRIORITY_NP.findgc(proc,len(processes))
 
     plt.bar(x, f_waiting, width = 0.25,label = "FCFS")
     plt.bar(x + 0.25, r_waiting, width = 0.25,label = "RR" )
-    plt.bar(x+0.5,np_waiting,width=0.25,label="NP")
     plt.xticks(x,processes)
 
    # ax.bar(X + 0.50, data[2], color = 'r', width = 0.25)
