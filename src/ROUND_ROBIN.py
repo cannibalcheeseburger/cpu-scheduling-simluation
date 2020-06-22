e6f8e5bf02fbe53bc2633c9c01cbac8fe9efac9d
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2020-06-22 16:03:20 +0530

    added roundrobin per quantum

--- src/ROUND_ROBIN.py
+++ src/ROUND_ROBIN.py
@@ -1,74 +1,97 @@
 import matplotlib.pyplot as plt
+from statistics import mean
+
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
 
-def findWaitingTime(processes, n, bt,  
-                         wt, quantum):  
-    rem_bt = [0] * n 
+def findWaitingTime(processes, n, burst_time, waiting_time, quantum):  
+    rem_burst_time = [0] * n 
 
     for i in range(n):  
-        rem_bt[i] = bt[i] 
+        rem_burst_time[i] = burst_time[i] 
     t = 0
     while(1): 
         done = True
         for i in range(n): 
-            if (rem_bt[i] > 0) : 
+            if (rem_burst_time[i] > 0) : 
                 done = False 
                   
-                if (rem_bt[i] > quantum) : 
+                if (rem_burst_time[i] > quantum) : 
                     t += quantum  
-                    rem_bt[i] -= quantum  
+                    rem_burst_time[i] -= quantum  
                 else: 
-                    t = t + rem_bt[i]  
-                    wt[i] = t - bt[i]  
-                    rem_bt[i] = 0
+                    t = t + rem_burst_time[i]  
+                    waiting_time[i] = t - burst_time[i]  
+                    rem_burst_time[i] = 0
         if (done == True): 
             break
               
 
-def findTurnAroundTime(processes, n, bt, wt, tat): 
+def findTurnAroundTime(processes, n, burst_time, waiting_time, tat): 
     for i in range(n): 
-        tat[i] = bt[i] + wt[i]  
+        tat[i] = burst_time[i] + waiting_time[i]  
   
   
 
-def findavgTime(processes, n, bt,arrival_time, quantum):  
-    wt = [0] * n 
+def findavgTime(processes, n, burst_time,arrival_time, quantum):  
+    waiting_time = [0] * n 
     tat = [0] * n  
     compl_time = [0]*len(processes)
 
-    findWaitingTime(processes, n, bt,  wt, quantum)  
+    findWaitingTime(processes, n, burst_time,  waiting_time, quantum)  
 
-    findTurnAroundTime(processes, n, bt, wt, tat)  
-  
-    print("Processes    Burst Time     Waiting",  "Time    Turn-Around Time    Completion Time") 
-    total_wt = 0
+    findTurnAroundTime(processes, n, burst_time, waiting_time, tat)    
+    total_waiting_time = 0
     total_tat = 0
     for i in range(n): 
-  
-        total_wt = total_wt + wt[i]  
+        total_waiting_time = total_waiting_time + waiting_time[i]  
         total_tat = total_tat + tat[i]  
         compl_time[i] = arrival_time[i] + tat[i]
-        print(" ", processes[i] , "\t\t", bt[i], "\t\t", wt[i], "\t\t", tat[i],"\t\t",compl_time[i]) 
-  
-    print("\nAverage waiting time = %.5f "%(total_wt /n) ) 
-    print("Average turn around time = %.5f "% (total_tat / n))  
-    print('Throughput = ', len(processes)/ compl_time[len(processes)-1])
-    print('Average Job elapsed time = ',total_tat/len(processes))
-    return wt, tat ,compl_time
+    return waiting_time, tat ,compl_time
       
+
 
 def plot_graph_normal(processes,waiting_time,compl_time,turn_around_time):
     
-    plt.plot(processes,waiting_time,label = "Waiting time")
-    plt.plot(processes,compl_time,label = "Completion time")
-    plt.plot(processes,turn_around_time,label = "Turnaround Time")
-    plt.text(4,4,'Throughput = %.5f'  % (len(processes)/ compl_time[len(processes)-1]))
-    plt.title("Round Robin Algo")
-    plt.xlabel("Processes")
-    plt.ylabel("Time Units")
-    plt.legend()
-    plt.savefig('./output/ROUND_ROBIN_output.png')
-    plt.show()
+    ax1.plot(processes,waiting_time,label = "waiting_time")
+    ax1.plot(processes,compl_time,label = "Completion time")
+    ax1.plot(processes,turn_around_time,label = "Turnaround Time")
+
+    ax1.legend()
+   # plt.savefig('../output/ROUND_ROBIN_output.png')
+
+
+def plot_graph_quantum(processes, burst_time, arrival_time):
+    throughput_quantum = []
+    avg_waiting_quantum = []
+    avg_turnaround_quantum = []
+    avg_completion_quantum =[]
+    for n in range(1,10):
+        waiting_time,turn_around_time,compl_time =  findavgTime(processes, len(processes), burst_time, arrival_time, n) 
+        throughput_quantum.append(len(processes)/ compl_time[len(processes)-1])
+        avg_turnaround_quantum.append(mean(turn_around_time))
+        avg_completion_quantum.append(mean(compl_time))
+        avg_waiting_quantum.append(mean(waiting_time))
+
+    rang = list(range(1,10))
+    ax2.plot(rang,throughput_quantum,label="Throughput")
+    ax2.plot(rang,avg_waiting_quantum,label="Average waiting_time")
+    ax2.plot(rang,avg_completion_quantum,label="Average Completion Time")
+    ax2.plot(rang,avg_turnaround_quantum,label="Average Turn Around Time")
+
+    ax2.legend()
+
+
+
+def print_details(processes,burst_time,waiting_time,compl_time,tat):
+    print("Processes    Burst Time     Waiting  Time    Turn-Around Time    Completion Time") 
+    for i in range(len(processes)):
+           print(" ", processes[i] , "\t\t", burst_time[i], "\t\t", waiting_time[i], "\t\t", tat[i],"\t\t",compl_time[i]) 
+    print("\nAverage waiting_time = %.5f "%(sum(waiting_time) /len(processes)) ) 
+    print("Average turn around time = %.5f "% (sum(tat) / len(processes)))  
+    print('Throughput = ', len(processes)/ compl_time[len(processes)-1])
+    print('Average Job elapsed time = ',sum(tat)/len(processes))
+
 
 
 def round_robin():
@@ -86,9 +109,13 @@ def round_robin():
 
 
     # Time quantum  
-    quantum = 6;  
+    quantum = 2;  
     waiting_time,turn_around_time,compl_time =  findavgTime(processes, len(processes), burst_time, arrival_time, quantum) 
+    print_details(processes,burst_time,waiting_time,compl_time,turn_around_time)
     plot_graph_normal(processes,waiting_time,compl_time,turn_around_time)
+    plot_graph_quantum(processes, burst_time, arrival_time)
+    plt.savefig("./output/ROUND_ROBIN.png")
+    plt.show()
     input()
 
 # Driver code  
